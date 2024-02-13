a209dcade376937dae01b50c3db8912973d9d76a
Albim360 <alberto.zappala@icloud.com>
2024-02-13 01:37:11 +0100

    FIX Button and title on hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="hero">
     <div class="blur-background"></div>
-    <div class="container-fluid">
-      <div class="row justify-content-center align-items-center vh-100">
-        <div class="col-12 text-center z-index-master">
-          <h1 class="welcome letter-spacing-3">
-            Welcome to
-            <span class="title letter-spacing-3">
-              <span class="black">Comfy <span class="orange-bold">House</span></span>
-            </span>
-          </h1>
-          <button class="fs-5">EXPLORE</button>
-        </div>
+    <div class="container-fluid d-flex justify-content-center align-items-center" style="height: 60vh;">
+      <div class="text-center z-index-master">
+        <h1 class="welcome letter-spacing-3 mb-4">
+          Welcome to
+          <span class="title letter-spacing-3">
+            <span class="black">Comfy <span class="orange-bold">House</span></span>
+          </span>
+        </h1>
+        <button class="fs-5">EXPLORE</button>
       </div>
     </div>
   </div>
@@ -24,14 +22,10 @@ export default {
 </script>
 
 <style scoped>
-.hero {
-  position: relative;
-}
-
 
 .welcome {
   font-weight: bold;
-  color: #fff
+  color: #fff;
 }
 
 </style>
