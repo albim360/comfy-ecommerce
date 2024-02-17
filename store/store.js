ba6cb11c4f324be6c78e77f6b8ab9d4c4ca206fe
Albim360 <alberto.zappala@icloud.com>
2024-02-18 00:51:45 +0100

    FEAT formatting store.js

--- store/store.js
+++ store/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
   state() {
@@ -13,8 +13,10 @@ export default createStore({
     CLEAR_CART(state) {
       state.cartItems = [];
     },
-    REMOVE_FROM_CART(state, productId) { 
-      const index = state.cartItems.findIndex(item => item.sys.id === productId);
+    REMOVE_FROM_CART(state, productId) {
+      const index = state.cartItems.findIndex(
+        (item) => item.sys.id === productId
+      );
       if (index !== -1) {
         state.cartItems.splice(index, 1);
       }
@@ -22,19 +24,22 @@ export default createStore({
   },
   actions: {
     addToCart({ commit }, product) {
-      commit('ADD_TO_CART', product);
+      commit("ADD_TO_CART", product);
       return product;
     },
     clearCart({ commit }) {
-      commit('CLEAR_CART');
+      commit("CLEAR_CART");
     },
-    removeFromCart({ commit }, productId) { 
-      commit('REMOVE_FROM_CART', productId);
+    removeFromCart({ commit }, productId) {
+      commit("REMOVE_FROM_CART", productId);
     },
   },
   getters: {
     cartTotal: (state) => {
-      return state.cartItems.reduce((total, item) => total + item.fields.price, 0);
+      return state.cartItems.reduce(
+        (total, item) => total + item.fields.price,
+        0
+      );
     },
     cartItemCount: (state) => {
       return state.cartItems.length;
